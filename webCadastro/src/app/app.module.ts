1a46f1a28753b091c8e77f73a3cd04ed3f8edc26
alvacirjunior01 <alvacir_juninho@hotmail.com>
2021-08-19 23:41:11 -0300

    Header

--- webCadastro/src/app/app.module.ts
+++ webCadastro/src/app/app.module.ts
@@ -1,30 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSidenavModule } from '@angular/material/sidenav';
-import {MatToolbarModule } from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 import { FlexLayoutModule } from "@angular/flex-layout";
-import { LayoutComponent } from './layout/layout.component';
 import { CommonModule } from '@angular/common';
 import { MatTabsModule } from '@angular/material/tabs';
-
 import { MatListModule } from '@angular/material/list';
 import { MatMenuModule } from '@angular/material/menu';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
     FooterComponent,
-    LayoutComponent,
-  
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
@@ -39,9 +34,6 @@ import { MatMenuModule } from '@angular/material/menu';
     MatTabsModule,
     MatListModule,
     MatMenuModule
-
-
-
   ],
   providers: [],
   bootstrap: [AppComponent]
--- webCadastro/src/app/header/header.component.ts
+++ webCadastro/src/app/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { StringMap } from '@angular/compiler/src/compiler_facade_interface';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,15 +8,52 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  @Output() public sidenavToggle = new EventEmitter();
+  activeClass: string = 'active';
 
-  constructor() { }
+  constructor() {
+    
+   }
 
   ngOnInit(): void {
+    let mobileMenu = document.querySelector('.mobile-menu');
+    let navList = document.querySelector(".nav-list")!;
+    let navLink = document.querySelectorAll(".nav-list li")!;
+    
+  
+
+    if(mobileMenu){
+      this.addClickEvent(mobileMenu, navList, navLink);
+    }
+
+ 
   }
 
-  public onToggleSidenav = () => {
-    this.sidenavToggle.emit();
+
+  addClickEvent(mob:Element, nav:Element,links: NodeListOf<Element>){
+    mob.addEventListener("click", () => nav.classList.toggle(this.activeClass));
+    this.animateLinks(links);
   }
 
-}
+   animateLinks(links: any) {
+     links.forEach((link:HTMLElement, i:number) => {
+       
+       link.style.animation?(link.style.animation = ""):(link.style.animation =`navigator 0.5s ease forwards 0.9s`);
+
+       console.log(i);
+     } )
+
+     
+    
+   }
+
+
+  
+    
+  }
+  
+
+
+
+  
+
+
